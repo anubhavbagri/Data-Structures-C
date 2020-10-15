7515d9cbe8aa8f2b2a1fb5bc2e30d7e9d6ebf395
anubhavbagri <anubhav.199302029@muj.manipal.edu>
2020-10-16 03:02:10 +0530

    added insertAtEnd

--- Linkedlist Applications & Programs/AlternativeSplit.c
+++ Linkedlist Applications & Programs/AlternativeSplit.c
@@ -8,6 +8,30 @@ struct Node
 };
 typedef struct Node node;
 
+void insertAtEnd(node **start, int d)
+{
+    /* 1. allocate new node. */
+    node *newNode = (node *)malloc(sizeof(node));
+    /* 2. put the data into newNode. */
+    newNode->data = d;
+    /* 3. this new node is going to be the last node, so make the next null. */
+    newNode->next = NULL;
+    /* 4. if the linked list is empty, then make the newNode as start. */
+    if (*start == NULL)
+    {
+        *start = newNode;
+        return;
+    }
+    /*5. Else traverse till the last node */
+    node *last = *start;
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+    /*6. change the next of the last node */
+    last->next = newNode;
+}
+
 void alternateSplit(node *start, node **aRef, node **bRef)
 {
     node a;
